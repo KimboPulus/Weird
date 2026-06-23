f24ab00425af5c852919fc046fce3d2b84027e82
KimboPulus <maksymmalinowski@gmail.com>
2026-06-23 20:42:33 +0200

    Set up Java project

---
+++ src/main/java/com/kimbopulus/weird/Main.java
@@ -0,0 +1,10 @@
+package com.kimbopulus.weird;
+
+public final class Main {
+    private Main() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Weird terrarium starting soon.");
+    }
+}
