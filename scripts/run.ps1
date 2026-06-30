5e9e729b9fd8e110ac88ed64b09d2685d9ea18e3
KimboPulus <maksymmalinowski@gmail.com>
2026-06-30 02:18:58 +0200

    remove files

--- scripts/run.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-$root = Split-Path -Parent $PSScriptRoot
-& (Join-Path $PSScriptRoot "build.ps1")
-java -cp (Join-Path $root "out") com.kimbopulus.weird.Main
-
--- scripts/visual-check.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-$root = Split-Path -Parent $PSScriptRoot
-$testOutput = Join-Path $root "out\test"
-$imageOutput = Join-Path $root "out\visual-check.png"
-
-& (Join-Path $PSScriptRoot "check.ps1")
-
-java "-Djava.awt.headless=true" -cp "$($root)\out;$testOutput" com.kimbopulus.weird.VisualSmokeCheck $imageOutput
